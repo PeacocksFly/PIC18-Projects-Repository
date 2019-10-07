2e30b4cc308fa5dd0fe1390af28ea4bcd29f4bcb
PeacocksFly <peacocks01@gmail.com>
2019-10-07 20:34:46 +0200

    Bipolar stepper motor

---
+++ bipolar_stepper_motor.c
@@ -0,0 +1,16 @@
+/*
+ * File:   bipolar_stepper_motor.c
+ * Author: Quentin
+ *
+ * Created on 07 October 2019, 20:31
+ */
+
+
+#include <xc.h>
+
+#pragma config MCLRE= EXTMCLR, WDTEN=OFF, FOSC=HSHP
+#define _XTAL_FREQ 8000000
+
+void main(void) {
+    return;
+}
