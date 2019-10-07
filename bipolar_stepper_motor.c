d6d35b74d69e384abfdc7166b8818451ed464413
PeacocksFly <peacocks01@gmail.com>
2019-10-07 21:04:58 +0200

    Bipolar stepper motor - added control

--- bipolar_stepper_motor.c
+++ bipolar_stepper_motor.c
@@ -7,10 +7,44 @@
 
 
 #include <xc.h>
+#include <stdint.h>
 
 #pragma config MCLRE= EXTMCLR, WDTEN=OFF, FOSC=HSHP
 #define _XTAL_FREQ 8000000
+#define REVS 10
+#define CLOCKWISE 1
+#define ANTICLOCKWISE -1
+
+uint8_t steps[]={0x01,0x04,0x02,0x08};
+
+void Bipolar_Motor_Turn(uint16_t, uint8_t*, int8_t);
 
 void main(void) {
+    
+    ANSELD = 0;
+    TRISD = 0;
+    
+    Bipolar_Motor_Turn(REVS, &steps[0], CLOCKWISE);  
+    __delay_ms(5000);
+    Bipolar_Motor_Turn(REVS, &steps[3], ANTICLOCKWISE);
+    
+    while(1);
+    
     return;
+}
+
+
+void Bipolar_Motor_Turn(uint16_t revs, uint8_t* start, int8_t dir)
+{
+    uint8_t* temp;
+    for(uint16_t i=0; i<revs;i++)
+    {
+        temp = start;
+        for(uint8_t j=0; j<4;j++)
+        {
+            PORTD = *temp;
+            temp += dir;
+            __delay_ms(250);
+        }
+    }
 }
